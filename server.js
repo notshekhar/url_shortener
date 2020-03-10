e77d908ec4f5de7f5d09da75868f94f5200d6752
Glitch (shorte) <none>
2020-03-10 04:17:17 +0000

    🐄🐀 Checkpoint ./server.js:2628414/1718

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get("/getAllurls", (req, res) => {
     })
 })
 app.get("/:surl", (req, res) => {
-  
+    console.log(whichBrowser(req))
     let shortURL = req.params.surl
     getURL(shortURL, url => {
         updateCount(shortURL, count => {
@@ -87,3 +87,42 @@ app.post("/login", (req, res) => {
         }
     })
 })
+
+
+function whichBrowser(req){
+    let ua = req.headers['user-agent'];
+    let a = {};
+
+    if (/mobile/i.test(ua))
+        a.mobile = true;
+
+    if (/like Mac OS X/.test(ua)) {
+        a.iOS = /CPU( iPhone)? OS ([0-9\._]+) like Mac OS X/.exec(ua)[2].replace(/_/g, '.');
+        a.iPhone = /iPhone/.test(ua);
+        a.iPad = /iPad/.test(ua);
+    }
+
+    if (/Android/.test(ua))
+        a.android = /Android ([0-9\.]+)[\);]/.exec(ua)[1];
+
+    if (/webOS\//.test(ua))
+        a.webOS = /webOS\/([0-9\.]+)[\);]/.exec(ua)[1];
+
+    if (/(Intel|PPC) Mac OS X/.test(ua))
+        a.mac = /(Intel|PPC) Mac OS X ?([0-9\._]*)[\)\;]/.exec(ua)[2].replace(/_/g, '.') || true;
+
+    if (/Windows NT/.test(ua))
+        a.windows = /Windows NT ([0-9\._]+)[\);]/.exec(ua)[1];
+  
+    if( /firefox/i.test(ua) )
+      a.browser = 'firefox';
+    else if( /chrome/i.test(ua) )
+      a.browser = 'chrome';
+    else if( /safari/i.test(ua) )
+      a.browser = 'safari';
+    else if( /msie/i.test(ua) )
+      a.browser = 'msie';
+    else
+      a.browser = 'unknown';
+    return a 
+}
